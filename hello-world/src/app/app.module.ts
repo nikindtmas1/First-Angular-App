a931cd7dfac95cb2a29c79d115a4e08443fd41b8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-05 11:44:50 +0200

    Added modules

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,3 +2,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModel } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { CoursesComponent } from "./courses.component";
+
+@NgModel({
+    declarations: [
+        AppComponent,
+        CoursesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+
+export class AppModule {}
